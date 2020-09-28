0f9f4a2dad870e18ade381db3fe17cae3dc471ad
Danil Besputin <besputindanil@yandex.ru>
2020-09-28 11:22:41 +0400

    feat: add solution to count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,3 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {};
+module.exports = function countCats(matrix) {
+  const catArray = matrix
+    .reduce((current, next) => current.concat(next), [])
+    .filter((item) => item === "^^");
+
+  if (!catArray.length) {
+    return 0;
+  } else {
+    return catArray.length;
+  }
+};
